5cb542163db63449d1e35775f0402d91667d94ed
tarinagarwal <theultimatemania64@gmail.com>
2025-05-15 20:39:55 +0530

    fix: update app.run to use environment variable for port configuration

--- server/app.py
+++ server/app.py
@@ -138,4 +138,5 @@ def page_not_found(e):
     return util.apology("Page not found", 404)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
